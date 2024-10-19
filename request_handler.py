1c0c21a4f24120838b0c4a52024e71f1f1478e0f
asparadise33 <abby.paradise@gmail.com>
2024-10-19 10:49:50 -0500

    put, post and delete methods added to animals

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_single_customer, get_all_customers, get_all_employees, get_single_employee
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_single_customer, get_all_customers, get_all_employees, get_single_employee, create_animal, delete_animal, update_animal
 
 
 
@@ -117,16 +117,32 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(response).encode())  
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
+
     def do_POST(self):
         """Handles POST requests to the server
         """
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_animal).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
@@ -134,8 +150,34 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_PUT(self):
         """Handles PUT requests to the server
         """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
 
+    # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+    def do_DELETE(self):
+    # Set a 204 response code
+      self._set_headers(204)
+
+    # Parse the URL
+      (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+      if resource == "animals":
+        delete_animal(id)
+
+    # Encode the new animal and send in response
+      self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
