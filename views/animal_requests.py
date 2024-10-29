2c3625a12649825d91ce23f222ede60414a8b694
asparadise33 <abby.paradise@gmail.com>
2024-10-28 21:26:14 -0500

    joined customers and location on animal sql query

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -2,6 +2,7 @@ import sqlite3
 import json
 from models import Animal
 from models import Location
+from models import Customer
 
 ANIMALS = [
     {
@@ -49,10 +50,16 @@ def get_all_animals():
             a.location_id,
             a.customer_id,
             l.name location_name,
-            l.address location_address                            
+            l.address location_address,
+            c.name customer_name,
+            c.address customer_address,
+            c.email customer_email,
+            c.password customer_password                                                        
         FROM Animal a
         JOIN Location l
-           ON l.id = a.location_id                                
+           ON l.id = a.location_id
+        JOIN Customer c
+           ON c.id = a.customer_id                                                  
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -73,8 +80,11 @@ def get_all_animals():
                             row['customer_id'])
             # Create a Location instance from the current row
             location = Location(row['id'], row['location_name'], row['location_address'])
+            #add a Customer instance from the current row
+            customer = Customer(row['id'], row['customer_name'], row['customer_address'], row['customer_email'],row['customer_password'])
             # Add the dictionary representation of the location to the animal
             animal.location = location.__dict__
+            animal.customer = customer.__dict__
             # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__) # see the notes below for an explanation on this line of code.
 
